0edc21465cf9189cd8979cd1ebd53dee35da35a0
Filippos Cjapi <filipposcjapi@Filipposs-MacBook-Pro.local>
2019-03-27 13:41:14 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+    
+    // Show Loader
+    document.getElementById('loading').style.display = 'block';
+
+    // setTimeout(calculateResults, 2000);
+    calculateResults();
+    e.preventDefault();
+});
+
+// Clculate Results
+function calculateResults(e){
+    console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principle = parseFloat(amount.value);
+    const calculateInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculateInterest, calculatedPayments);
+    const monthly = (principle*x*calculateInterest)/(x-1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly*calculatedPayments)-principle).toFixed(2);
+
+        //Hide Results
+        document.getElementById('results').style.display = 'block';
+
+        // Hide the Loading icon
+        document.getElementById('loading').style.display = 'none';
+    }else {
+        
+        showError('Please Check Your Numbers And Try Again');
+    }
+}
+// Show an Error
+function showError(error){
+    
+    //Show Results
+    document.getElementById('results').style.display = 'none';
+
+    // Hide the Loading icon
+    document.getElementById('loading').style.display = 'none';
+
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above the heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 second
+    setTimeout(clearError, 3000);
+}
+function clearError(){
+    document.querySelector('.alert').remove();
+};
